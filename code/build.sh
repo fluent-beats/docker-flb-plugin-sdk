0f0ea4dce82756167185209e14c964728cea3535
cadu.goncalves <cadu.goncalves@gmail.com>
2023-03-22 22:01:19 -0300

    Changed build version

--- code/build.sh
+++ code/build.sh
@@ -4,4 +4,4 @@ set -e
 cd $(dirname $0)
 
 docker build -f Dockerfile.sdk -t fluent-beats/fluent-bit-sdk .
-docker build -f Dockerfile.flb -t fluent-beats/fluent-bit-plugin-dev --build-arg FLB_VERSION=1.8.4 .
+docker build -f Dockerfile.flb -t fluent-beats/fluent-bit-plugin-dev --build-arg FLB_VERSION=1.8.11 .
